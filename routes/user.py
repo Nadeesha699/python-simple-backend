ae2e83c5458f76bf8399193963ed79a0d8b1b140
Nadeesha Ruwandima <114317388+Nadeesha699@users.noreply.github.com>
2025-02-13 02:04:27 +0530

    saved

--- routes/user.py
+++ routes/user.py
@@ -9,38 +9,53 @@ user_route = Blueprint('user_routes',__name__)
 
 @user_route.route('/get-all',methods=['GET'])
 def get_all_users():
-  cursor.execute('SELECT * FROM user')
-  user =  cursor.fetchall()
-  return jsonify({"data":user,"success":True,})
+  try:
+    cursor.execute('SELECT * FROM user')
+    user =  cursor.fetchall()
+    return jsonify({"data":user,"success":True})
+  except Exception as e:
+    return jsonify({"message":e.message,"success":False})
 
 
 @user_route.route('/get/by-id/<int:id>', methods=['GET'])
 def get_user_by_id(id):
-  cursor.execute('SELECT * FROM user WHERE ID = %s',(id,))
-  users = cursor.fetchone()
-  return jsonify({"data":users,"success":True})
+  try:
+    cursor.execute('SELECT * FROM user WHERE ID = %s',(id,))
+    users = cursor.fetchone()
+    return jsonify({"data":users,"success":True})
+  except Exception as e:
+    return jsonify({"message":e.message,"success":False}),500
 
 @user_route.route('/create',methods=['POST'])
 def set_user():
-  data = request.json
-  name = data.get("Name")
-  age = data.get("Age")
-  cursor.execute('INSERT INTO user (Name,Age)VALUES(%s,%s)',(name,age))
-  con.commit()
-  return jsonify({"success":True})
+  try:
+    data = request.json
+    name = data.get("Name")
+    age = data.get("Age")
+    cursor.execute('INSERT INTO user (Name,Age)VALUES(%s,%s)',(name,age))
+    con.commit()
+    return jsonify({"success":True,"message":"User created successfully"})
+  except Exception as e:
+    return jsonify({"message":e.message,"success":False}),500
 
 @user_route.route('/edit/by-id/<int:id>',methods=['PUT'])
 def update_user_by_id(id):
-  data = request.json
-  name = data.get('Name')
-  age = data.get('Age')
-  cursor.execute('UPDATE user SET Name = %s, Age =%s WHERE ID = %s',(name,age,id,))
-  con.commit()
-  return jsonify({"success":True})
+  try:
+    data = request.json
+    name = data.get('Name')
+    age = data.get('Age')
+    cursor.execute('UPDATE user SET Name = %s, Age =%s WHERE ID = %s',(name,age,id,))
+    con.commit()
+    return jsonify({"success":True,"message":"User updated successfully"})
+  except Exception as e:
+    return jsonify({"message":e.message,"success":False}),500
 
 
 @user_route.route('/delete/by-id/<int:id>',methods=['DELETE'])
 def delete_by_id(id):
-  cursor.execute('DELETE FROM user WHERE ID = %s',(id,))
-  con.commit()
-  return jsonify({"success":True})
+  try:
+    cursor.execute('DELETE FROM user WHERE ID = %s',(id,))
+    con.commit()
+    return jsonify({"success":True,"message":"User deleted successfully"})
+  except Exception as e:
+    return jsonify({"message":e.message,"success":False}),500
